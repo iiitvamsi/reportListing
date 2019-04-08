771cf4b3a5e85dd89502cf525a6e41fd8afca0cc
Krishnavamsi <bendalamkrishnavamsi@gmail.com>
2019-04-08 18:49:37 +0530

    Modal modifications

--- src/app/reportmaster/reportmaster.component.ts
+++ src/app/reportmaster/reportmaster.component.ts
@@ -17,24 +17,24 @@ export class ReportmasterComponent implements OnInit {
   ngOnInit() {
     this.getReportData();
 
-    var modal = document.getElementById('myModal');
+    // var modal = document.getElementById('myModal');
 
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("close")[0];
+    // var btn = document.getElementById("myBtn");
+    // var span = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
+    // btn.onclick = function() {
+    //   modal.style.display = "block";
+    // }
 
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
 
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // }
 
   }
 
