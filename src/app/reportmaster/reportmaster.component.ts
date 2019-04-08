3edfbb9610958a2d9c058bb88e4d1e82ceb21568
krishnavamsi <krishnavamsib@detecttechnologies.com>
2019-04-08 14:11:06 +0530

    Modal is added

--- src/app/reportmaster/reportmaster.component.ts
+++ src/app/reportmaster/reportmaster.component.ts
@@ -16,6 +16,26 @@ export class ReportmasterComponent implements OnInit {
 
   ngOnInit() {
     this.getReportData();
+
+    var modal = document.getElementById('myModal');
+
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
   }
 
   getReportData(){
